42a18b206622a62c9c91a6eee5c8cfcef434245c
Aoaoshy <13613086989@163.com>
2024-07-23 22:46:43 +0800

    完成了爪哇例2.1

---
+++ src/chapter1/Java2_1.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class Java2_1 {
+    public static void main(String[] args) {
+        int num = 10;   //定义变量
+        num = 30;   //改变变量
+        System.out.println("num 的内容是 " + num);
+        System.out.println("num * num = " + num * num);
+    }
+}
